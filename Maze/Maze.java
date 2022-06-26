f7746a33d51dff3f3feebc7d65166b75c4797754
smrrazavian <smrrazavian@outlook.com>
2022-06-27 03:32:30 +0430

    Just some code

---
+++ Maze/Maze.java
@@ -0,0 +1,43 @@
+package Maze;
+
+public class Maze {
+    // ----------------------- Variables -----------------------
+    private int rowNumber;
+    private int colNumber;
+
+    // ---------------------------------------------------------
+    /**
+     * @return the rowNumber
+     */
+    public int getRowNumber() {
+        return rowNumber;
+    }
+
+    /**
+     * @param rowNumber the rowNumber to set
+     */
+    public void setRowNumber(int rowNumber) {
+        this.rowNumber = rowNumber;
+    }
+
+    /**
+     * @return the colNumber
+     */
+    public int getColNumber() {
+        return colNumber;
+    }
+
+    /**
+     * @param colNumber the colNumber to set
+     */
+    public void setColNumber(int colNumber) {
+        this.colNumber = colNumber;
+    }
+
+    public Maze(int y, int x) {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
